f9ca9207977cfb752d11a4c5d9f830de1d1f7ee1
Quent_DLM <qdelameilleure@gmail.com>
2023-01-12 15:24:01 +0100

    second el

---
+++ dom_recuperation_elements_part1_exercice2/main.js
@@ -0,0 +1,15 @@
+let myBody = document.getElementsByTagName("body")
+let firstEle = myBody[0].childNodes[1]
+console.log(firstEle)
+
+let lastEle = myBody[0].childNodes[childNodes.lenght - 1]
+console.log(lastEle)
+
+let div = document.getElementsByTagName("div")
+let firstDiv = div[0]
+let exo4 = div[0].childNodes
+console.log(exo4)
+
+let firstLi = document.getElementsByTagName("li")[0];
+let secondLi = firstLi.nextElementSibling;
+console.log(secondLi);
